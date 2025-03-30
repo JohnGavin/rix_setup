22d40821e03caba008ae0fcf65da4329b8898dab
John Gavin <john.b.gavin@gmail.com>
2025-03-30 13:16:56 +0100

    renamed .Rprofile_empty (to help enforce --pure)

--- default.sh
+++ default.sh
@@ -1,7 +1,9 @@
 # ./default.sh
 # sudo rm -rf /private/tmp/nix-shell-* /tmp/Rtmp* && nix-collect-garbage -d
 
-echo "from rix only (default_R_nix_only.nix)\n\tto all pkgs\n\t(default_sym.R -> default.R -> default.nix)" && \
+echo "from rix only (via env default_R_nix_only.nix)
+    \tto all R pkgs
+    \t(default_sym.R -> default.R -> env default.nix)\n" && \
 nix-shell \
     --pure \
     --keep GITHUB_PAT \
@@ -9,8 +11,8 @@ nix-shell \
         "Rscript --vanilla default_sym.R" \
     default_R_nix_only.nix
 
-echo "\n\nrestart (all packages) (./default.nix + positron)" && \
-#    --pure \
+echo "\n\nrestart (all packages) (env ./default.nix + positron)\n" && \
+echo "\tNB: ignoring --pure \ # to avoid ~/.Rprofile but ./.Rprofile should do this already?\n" && \
 nix-shell \
     --keep GITHUB_PAT \
     --command \
@@ -19,12 +21,12 @@ nix-shell \
         export R_LIBS_USER='.' && \
         echo 'R_LIBS="" to enforce --pure' && \
         export R_LIBS='' && \
-        echo 'do not call ~/.Rprofile to enforce --pure' && \
+        echo 'to help enforce --pure: do NOT call ~/.Rprofile i.e. call ./.Rprofile_empty instead' && \
         touch ./empty_rprofile && \
         export R_PROFILE_USER=./empty_rprofile && \
         positron \
     " \
     default.nix
-    # --command "unset R_LIBS_USER && positron"
-    # Sys.getenv("R_LIBS_USER") ; Sys.unsetenv("R_LIBS_USER")
     # --argstr myVar "some value" -p hello --run 'echo $myVar'
+    # --command "unset R_LIBS_USER"
+    # Sys.getenv("R_LIBS_USER") ; Sys.unsetenv("R_LIBS_USER") ; Sys.getenv("R_LIBS_USER")
