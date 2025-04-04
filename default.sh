7dad8bcc0417b7ae41b59fe5b7e8d1bf8290f84b
John Gavin <john.b.gavin@gmail.com>
2025-04-04 12:28:18 +0100

    added back --pure

--- default.sh
+++ default.sh
@@ -11,22 +11,28 @@ nix-shell \
         "Rscript --vanilla default_sym.R" \
     default_R_nix_only.nix
 
-echo "\n\nrestart (all packages) (env ./default.nix + positron)\n" && \
-echo "\tNB: ignoring --pure \ # to avoid ~/.Rprofile but ./.Rprofile should do this already?\n" && \
+echo "\n\nrestart (all packages) (env ./default.nix + positron)" && \
+echo "\tNB: ignoring --pure \ # to avoid ~/.Rprofile\n\t\tbut ./.Rprofile should do this already?" && \
 nix-shell \
+    --pure \
     --keep GITHUB_PAT \
+    --argstr myVar "some value" \
     --command \
     " \
-        echo 'R_LIBS_USER="." to enforce --pure' && \
-        export R_LIBS_USER='.' && \
-        echo 'R_LIBS="" to enforce --pure' && \
-        export R_LIBS='' && \
-        echo 'to help enforce --pure: do NOT call ~/.Rprofile i.e. call ./.Rprofile_empty instead' && \
-        touch ./empty_rprofile && \
-        export R_PROFILE_USER=./empty_rprofile && \
-        positron \
+        positron ; echo $myVar ; return\
     " \
     default.nix
+    # NB: --pure do NOT
+        # allow writing to /tmp/private needed by cmdstanr
+        # Error 'ENOENT'??: no such file or directory, open '/private/tmp/nix-shell-82530-0/kallichore-4a46d10c.token'
     # --argstr myVar "some value" -p hello --run 'echo $myVar'
     # --command "unset R_LIBS_USER"
     # Sys.getenv("R_LIBS_USER") ; Sys.unsetenv("R_LIBS_USER") ; Sys.getenv("R_LIBS_USER")
+
+        # echo '  R_LIBS_USER="." to enforce --pure' && \
+        # export R_LIBS_USER='.' && \
+        # echo '  R_LIBS="" to enforce --pure' && \
+        # export R_LIBS='' && \
+        # echo '  to help enforce --pure: do NOT call ~/.Rprofile i.e. call ./.Rprofile_$
+        # touch ./empty_rprofile && \
+        # export R_PROFILE_USER=./empty_rprofile && \
